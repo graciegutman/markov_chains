11be1243acf442be77ba32a6ee6ec65fd95debf4
Hackbright Student <info@hackbrightacademy.com>
2014-02-12 18:21:09 -0800

    more Twitter functionality

--- MarkovClean.py
+++ MarkovClean.py
@@ -4,9 +4,16 @@ import random
 
 from string import join
 
+import auth
+
+import twitter 
+
 def make_list(corpus):
     word_list = corpus.split()
-    return word_list
+    san_list = []
+    for word in word_list:
+        san_list.append(word.strip('_--+=[]{}<>()'))
+    return san_list
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
@@ -25,16 +32,37 @@ def make_chains(corpus):
             chain_dict[temp_key].append(temp_value)
     return chain_dict
 
+def select_starter(chains):
+    """loop infinitely until we get to a key that starts with a capital letter"""
+    while True:
+        curr_key = random.choice(chains.keys())
+        if curr_key[0].istitle():
+            return curr_key
+
+def char_len_check(sentence):
+    """Returns True if sentence is under 140 characters"""
+    count = 0
+    for letter in sentence:
+        count += 1
+    if count < 141:
+        return True
+    return False
+
+def has_punctuation(word):
+    """If last character of a value matches ?, !, ., return False"""
+    if word[-1] in ["?", "!", "."]:
+        return False
+    return True
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
-    curr_key = random.choice(chains.keys())
+    curr_key = select_starter(chains)
     curr_list = [curr_key[0], curr_key[1]]
     val_word = curr_key[1]
 
-    while "." not in val_word and "!" not in val_word and "?" not in val_word:
+    while has_punctuation(val_word):
     # look up curr_key and get the value
         val_list = chains.get(curr_key)
     # if the selected value is the end of the text, value will be None type, 
@@ -47,16 +75,43 @@ def make_text(chains):
         curr_list.append(val_word)
         curr_key = (curr_key[1], val_word)
     sentence = join(curr_list)
-    return sentence 
+    return sentence
 
 def main():
-     args = sys.argv
-     filename = args[1]
-     input_text = open(filename).read()
+    """Runs main control flow"""
+    args = sys.argv
+    filename = args[1]
+    input_text = open(filename).read()
 
-     chain_dict = make_chains(input_text)
-     random_text = make_text(chain_dict)
-     print random_text
+    chain_dict = make_chains(input_text)
+    random_text = make_text(chain_dict)
+    while not char_len_check(random_text):
+       random_text = make_text(chain_dict)
+    return random_text
 
 if __name__ == "__main__":
     main()
+
+
+def tweet():  
+    api = twitter.Api(consumer_key=auth.twitter_key,
+                     consumer_secret=auth.api_secret,
+                     access_token_key=auth.access_token,
+                     access_token_secret=auth.access_token_secret)
+    
+    while True:
+        potential_tweet = main()
+        print potential_tweet
+        print "Do you want to tweet this?"
+        answer = raw_input("y/n? \n")
+        if answer == "y":
+            api.PostUpdates(potential_tweet)
+        elif answer == "q":
+            print "quitting"
+            return
+
+tweet()
+
+
+
+
